91f747875526fe5942220a716cbbbffd1de54fe9
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2024-02-03 13:06:02 +0600

    Update find the largest element from the user input.c
    Update conditional operator for largest element

--- week 2/find the largest element from the user input.c
+++ week 2/find the largest element from the user input.c
@@ -16,8 +16,8 @@ int arr[length];
     }
 
 int max = arr[0];
-    for(int i=0; i<sizeof(arr)/sizeof(arr[0]); i++){
-        if(arr[i]< max){
+    for(int i=1; i<sizeof(arr)/sizeof(arr[0]); i++){
+        if(arr[i]> max){
             max = arr[i];
         }
     }
